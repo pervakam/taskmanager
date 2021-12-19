afe294a8aa61ca4ff9a8e6bf3f010e39e4de86f1
pervakam <52745442+pervakam@users.noreply.github.com>
2021-12-19 18:14:40 +0300

    gh-pages

---
+++ precache-manifest.bc263326e25e13a6c288444b22f14645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5fb3ecb46f676dd2fc",
+    "url": "/taskmanager/css/app.de71f5ac.css"
+  },
+  {
+    "revision": "686e21af37df01c8e55b",
+    "url": "/taskmanager/css/chunk-vendors.d7bb60e3.css"
+  },
+  {
+    "revision": "9b536488da87f5d6ba6cf7b7c4341979",
+    "url": "/taskmanager/index.html"
+  },
+  {
+    "revision": "2e5fb3ecb46f676dd2fc",
+    "url": "/taskmanager/js/app.9eae60b9.js"
+  },
+  {
+    "revision": "686e21af37df01c8e55b",
+    "url": "/taskmanager/js/chunk-vendors.7b047332.js"
+  },
+  {
+    "revision": "bf16e86b03e82a0af5983f4853963b43",
+    "url": "/taskmanager/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taskmanager/robots.txt"
+  }
+]);
